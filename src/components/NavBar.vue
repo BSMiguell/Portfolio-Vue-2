3053aae1b83fc70d613080766f5266ec3b2b2b9a
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-15 09:33:35 -0300

    Primeiro commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app color="#112e41" dark flat class="px-12">
+    <v-btn color="white">
+      <v-icon color="#ff6f00" left class="mr-2">fas fa-signature</v-icon>BSM
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" color="#ff6f00">Home</v-btn>
+    <v-btn text @click="scroll('about')" color="white">About</v-btn>
+    <v-btn text @click="scroll('portfolio')" color="white">Portfolio</v-btn>
+    <v-btn text @click="scroll('services')" color="white">Services</v-btn>
+    <v-btn text @click="scroll('page')" color="white">Page</v-btn>
+    <v-btn text @click="scroll('biog')" color="white">Biog</v-btn>
+    <v-btn text @click="scroll('contact')" color="white">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
+
+<style></style>
